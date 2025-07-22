b3805c5aa09ba4b8d5430ea03a3599996e23cdf5
Varshikramg <166319079+Varshikramg@users.noreply.github.com>
2025-07-22 11:23:25 +0530

    Add .next/static/chunks/src_app_stocks_page_tsx_a993527f._.js 1753163604176

---
+++ .next/static/chunks/src_app_stocks_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f4354cdd._.js",
+  "static/chunks/node_modules_axios_lib_99999129._.js",
+  "static/chunks/node_modules_104d1069._.js",
+  "static/chunks/src_app_stocks_page_tsx_0ea95a79._.js"
+],
+    source: "dynamic"
+});
